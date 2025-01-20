14c83829dedade203dd7331e6d0eed0911830d4e
Miami <alex.miami.petrov@gmail.com>
2025-01-20 10:57:57 +0200

    cors_3

--- src/server.js
+++ src/server.js
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET,HEAD,PUT,PATCH,POST,DELETE',
   );
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
