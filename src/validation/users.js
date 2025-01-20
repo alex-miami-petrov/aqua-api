abb0d394506b06394f431322ccbfd0707795f266
Thunderman221 <119857501+Thunderman221@users.noreply.github.com>
2025-01-20 10:29:18 +0200

    add email to the update user

--- src/validation/users.js
+++ src/validation/users.js
@@ -14,8 +14,8 @@ const stringField = Joi.string().min(3).max(20);
 
 export const editUserSchema = Joi.object({
   name: stringField.optional(),
-
   gender: stringField.valid('woman', 'man').optional(),
+  email: stringField.email().optional(),
   weight: Joi.number().optional(),
   waterNorma: Joi.number().optional(),
   activeTime: Joi.number().optional(),
