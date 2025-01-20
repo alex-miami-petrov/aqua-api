f9584784afca2371ac36df1f05cc27fc570d36be
Miami <alex.miami.petrov@gmail.com>
2025-01-20 17:54:35 +0200

    cors_6

--- src/server.js
+++ src/server.js
@@ -29,20 +29,20 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', [
-    'http://localhost:5173',
-    'https://aqua-track-02-gr.vercel.app',
-  ]);
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,HEAD,PUT,PATCH,POST,DELETE',
-  );
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', [
+//     'http://localhost:5173',
+//     'https://aqua-track-02-gr.vercel.app',
+//   ]);
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   );
+//   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 app.use(express.json());
 app.use(cookieParser());
