dfde1022f87927300d88cc17be7ca5a39d4d145c
Miami <alex.miami.petrov@gmail.com>
2025-01-21 12:23:54 +0200

    OAuth_token

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -1,30 +1,60 @@
+// import { OAuth2Client } from 'google-auth-library';
+// import createHttpError from 'http-errors';
+
+// const googleOAuth2Client = new OAuth2Client({
+//   clientId: process.env.GOOGLE_AUTH_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+//   redirectUri: process.env.GOOGLE_AUTH_REDIRECT_URL,
+// });
+
+// export const generateAuthUrl = () =>
+//   googleOAuth2Client.generateAuthUrl({
+//     scope: [
+//       'https://www.googleapis.com/auth/userinfo.email',
+//       'https://www.googleapis.com/auth/userinfo.profile',
+//     ],
+//   });
+
+// export const validateCode = async (code) => {
+//   try {
+//     const response = await googleOAuth2Client.getToken(code);
+
+//     const ticket = await googleOAuth2Client.verifyIdToken({
+//       idToken: response.tokens.id_token,
+//     });
+
+//     return ticket;
+//   } catch (error) {
+//     if (
+//       error.response &&
+//       error.response.status >= 400 &&
+//       error.response.status <= 499
+//     ) {
+//       throw createHttpError(401, 'Unauthorized');
+//     }
+//     throw error;
+//   }
+// };
+
 import { OAuth2Client } from 'google-auth-library';
 import createHttpError from 'http-errors';
-
 const googleOAuth2Client = new OAuth2Client({
   clientId: process.env.GOOGLE_AUTH_CLIENT_ID,
   clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
   redirectUri: process.env.GOOGLE_AUTH_REDIRECT_URL,
 });
-
-export const generateAuthUrl = () =>
-  googleOAuth2Client.generateAuthUrl({
-    scope: [
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/userinfo.profile',
-    ],
-  });
-
-export const validateCode = async (code) => {
+export const validateIdToken = async (idToken) => {
   try {
-    const response = await googleOAuth2Client.getToken(code);
-
+    // Перевіряємо ID токен
     const ticket = await googleOAuth2Client.verifyIdToken({
-      idToken: response.tokens.id_token,
+      idToken, // Відправляємо ID токен безпосередньо
+      audience: process.env.GOOGLE_AUTH_CLIENT_ID, // Перевіряємо відповідність clientId
     });
-
-    return ticket;
+    // Витягуємо дані користувача з ID токена
+    const payload = ticket.getPayload();
+    return payload; // Повертаємо дані користувача (email, ім'я, фото тощо)
   } catch (error) {
+    // Обробка помилок
     if (
       error.response &&
       error.response.status >= 400 &&
